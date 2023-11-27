e469eb49e0ba39658ea9d7ba0609217149c82a62
DimitriDoru <152091503+DimitriDoru@users.noreply.github.com>
2023-11-27 10:36:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ yesBtn.addEventListener("click", () => {
   question.innerHTML = "Good to hear, what happend that it made it so good?";
   gif.src ="https://media.giphy.com/media/FDbIUZLwbCUS4786z3/giphy.gif";
   noBtn.style.display = "none";
-  const remainingBtn = document.querySelector('.btn-group button:not(.no-btn)');
+  const yremainingBtn = document.querySelector('.btn-group button:not(.no-btn)');
     // Center the remaining button by setting margin to auto
     remainingBtn.style.margin = 'auto';
 });
@@ -18,7 +18,7 @@ noBtn.addEventListener("click", () => {
   question.innerHTML = "If you want i can give you a hug baby.";
   jpg.src ="https://image.emojipng.com/467/12319467.jpg";
   yesBtn.style.display = "none";
-  const remainingBtn = document.querySelector('.btn-group button:not(.yes-btn)');
+  const nremainingBtn = document.querySelector('.btn-group button:not(.yes-btn)');
     // Center the remaining button by setting margin to auto
     remainingBtn.style.margin = 'auto';
 });
